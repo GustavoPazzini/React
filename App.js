7dc9ed2fd14031f22c9026d1aec6bde82567c169
GustavoPazzini <gustavo200699@hotmail.com>
2022-05-09 18:41:38 -0300

    update

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
         placeholder="Senha"
         secureTextEntry={true}
       />
-      <Button title="Entrar" />
+      <Button title="Entrar"/>
     </View>
   );
 }
